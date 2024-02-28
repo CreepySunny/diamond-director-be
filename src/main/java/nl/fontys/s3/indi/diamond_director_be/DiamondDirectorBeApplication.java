2837ef21d3a505b75a039b3f40e77cdb8d8bef80
Sidney Thiel <9028-I499902@users.noreply.git.fhict.nl>
2024-02-28 23:23:37 +0100

    Project Creation

---
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/DiamondDirectorBeApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.indi.diamond_director_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondDirectorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiamondDirectorBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fontys/s3/indi/diamond_director_be/DiamondDirectorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.indi.diamond_director_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondDirectorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
