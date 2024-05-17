015124d79b8e9e43d3ab96e2ad31bcd0ea1afc1b
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-05-17 12:54:22 +0200

    Added Login Controller

---
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/Controllers/LogInController.java
@@ -0,0 +1,28 @@
+package nl.fontys.s3.indi.diamond_director_be.Controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import nl.fontys.s3.indi.diamond_director_be.business.UserLogInUseCase;
+import nl.fontys.s3.indi.diamond_director_be.domain.Auth.LogInRequest;
+import nl.fontys.s3.indi.diamond_director_be.domain.Auth.LogInResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tokens")
+@RequiredArgsConstructor
+public class LogInController {
+    private final UserLogInUseCase userLogInUseCase;
+
+    @PostMapping
+    public ResponseEntity<LogInResponse> login(@Valid @RequestBody LogInRequest logInRequest) {
+        LogInResponse response = userLogInUseCase.userAuthenticate(logInRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
