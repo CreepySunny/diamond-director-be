233293573796b8dd826a8a434b9b297892659ee0
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-07 23:56:14 +0200

    DIAM-7 Reformat enums

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/domain/GameState/Enums/Bases.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/domain/GameState/Enums/Bases.java
@@ -1,8 +1,8 @@
 package nl.fontys.s3.indi.diamond_director_be.domain.GameState.Enums;
 
 public enum Bases {
-    first,
-    second,
-    third,
-    home
+    FIRST,
+    SECOND,
+    THIRD,
+    HOME
 }
