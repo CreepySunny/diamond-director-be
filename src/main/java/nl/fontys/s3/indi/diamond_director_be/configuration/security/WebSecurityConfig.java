e890f10adf6f8ec0cf44ba9f8fc43c7723792828
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-11 06:30:14 +0200

    Allow coach account creation

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/configuration/security/WebSecurityConfig.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/configuration/security/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig {
                         configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(registry ->
                         registry.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()                 // CORS pre-flight requests should be public
-                                .requestMatchers(HttpMethod.POST, "/player", "/tokens").permitAll() // Creating a student and login are public
+                                .requestMatchers(HttpMethod.POST, "/player", "/tokens", "/coach").permitAll() // Creating a student and login are public
                                 .anyRequest().authenticated()                                             // Everything else --> authentication required, which is Spring security's default behaviour
                 )
                 .exceptionHandling(configure -> configure.authenticationEntryPoint(authenticationEntryPoint))
