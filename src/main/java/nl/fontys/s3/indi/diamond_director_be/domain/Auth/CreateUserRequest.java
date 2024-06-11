afd743e8b8e22122fbc206fa450d30f163bd88fd
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-11 06:31:00 +0200

    Unused Import

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/domain/Auth/CreateUserRequest.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/domain/Auth/CreateUserRequest.java
@@ -1,7 +1,6 @@
 package nl.fontys.s3.indi.diamond_director_be.domain.Auth;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
