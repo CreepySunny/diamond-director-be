f87de4bdc2f5fc593fc17a81e4bb2647e912f620
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-11 06:31:23 +0200

    Find by User ID

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/GameRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/GameRepository.java
@@ -2,7 +2,19 @@ package nl.fontys.s3.indi.diamond_director_be.persistance;
 
 import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.GameEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameRepository extends JpaRepository<GameEntity, Long> {
-    void updateGame(GameEntity gameEntity);
+    @Query("SELECT g FROM GameEntity g " +
+            "JOIN g.homeTeam h " +
+            "JOIN h.coaches hc " +
+            "JOIN hc.userEntity hcu " +
+            "JOIN g.awayTeam a " +
+            "JOIN a.coaches ac " +
+            "JOIN ac.userEntity acu " +
+            "WHERE hcu.id = :userId OR acu.id = :userId")
+    List<GameEntity> findGamesByCoachId(@Param("userId") Long coachId);
 }
