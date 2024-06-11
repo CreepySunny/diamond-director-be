263482ae1f425f115483c94951173b3182e2f872
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-11 06:21:49 +0200

    Convert from Player Object to Entity

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/business/Converters/PlayerConverter.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/business/Converters/PlayerConverter.java
@@ -6,6 +6,20 @@ import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.PlayerEntity;
 public final class PlayerConverter {
     private PlayerConverter() {}
 
+
+    public static PlayerEntity convert(Player player) {
+        PlayerEntity playerEntity = new PlayerEntity();
+        playerEntity.setId(player.getId());
+        playerEntity.setFirstName(player.getFirstName());
+        playerEntity.setLastName(player.getLastName());
+        playerEntity.setHandedBats(player.getHanded_bats());
+        playerEntity.setHandedThrows(player.getHanded_throws());
+        playerEntity.setPosition(player.getPosition());
+        playerEntity.setDateOfBirth(player.getDateOfBirth());
+        playerEntity.setHeight(player.getHeight());
+        playerEntity.setWeight(player.getWeight());
+        return playerEntity;
+    }
     public static Player convert(PlayerEntity playerEntity) {
         return Player.builder()
                 .id(playerEntity.getId())
