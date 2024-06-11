2e656d9f1d13ec1a2a140616843754d064cd6a3f
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-11 06:30:26 +0200

    Find By Team Name

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/TeamRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/TeamRepository.java
@@ -4,6 +4,9 @@ import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
+    Optional<TeamEntity> findByTeamName(String teamName);
 }
