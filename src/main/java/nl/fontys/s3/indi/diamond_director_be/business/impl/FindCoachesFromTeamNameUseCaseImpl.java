6bb7134e0d1a190a2b1ffba67c5ea04fe9bbcbca
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 12:07:13 +0200

    Find Coaches from a team using team name

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/business/impl/FindCoachesFromTeamNameUseCaseImpl.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/business/impl/FindCoachesFromTeamNameUseCaseImpl.java
@@ -5,7 +5,6 @@ import nl.fontys.s3.indi.diamond_director_be.business.Converters.CoachConverter;
 import nl.fontys.s3.indi.diamond_director_be.business.Exceptions.NO_TEAM_FOUND_EXCEPTION;
 import nl.fontys.s3.indi.diamond_director_be.business.FindCoachesFromTeamNameUseCase;
 import nl.fontys.s3.indi.diamond_director_be.domain.Coach.Coaches;
-import nl.fontys.s3.indi.diamond_director_be.persistance.CoachRepository;
 import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.TeamEntity;
 import nl.fontys.s3.indi.diamond_director_be.persistance.TeamRepository;
 import org.springframework.stereotype.Service;
@@ -15,13 +14,12 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class FindCoachesFromTeamNameUseCaseImpl implements FindCoachesFromTeamNameUseCase {
-    private final CoachRepository coachRepository;
     private final TeamRepository teamRepository;
 
     @Override
     public List<Coaches> findCoachFromTeamName(String teamName) {
         TeamEntity foundTeam = teamRepository.findByTeamName(teamName).orElseThrow(NO_TEAM_FOUND_EXCEPTION::new);
-        return coachRepository.findCoachEntitiesByTeamIs(foundTeam).stream()
+        return foundTeam.getCoaches().stream()
                 .map(CoachConverter::convert)
                 .toList();
     }
