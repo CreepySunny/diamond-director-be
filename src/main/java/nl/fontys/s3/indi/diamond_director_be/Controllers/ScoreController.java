dc6edf7f1301f6c2595de5d296117a1d322c274c
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 02:23:22 +0200

    Send Notification

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/Controllers/ScoreController.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/Controllers/ScoreController.java
@@ -2,6 +2,9 @@ package nl.fontys.s3.indi.diamond_director_be.Controllers;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import nl.fontys.s3.indi.diamond_director_be.business.AddNewBaseballPlayUseCase;
+import nl.fontys.s3.indi.diamond_director_be.domain.GameState.Game;
+import nl.fontys.s3.indi.diamond_director_be.domain.GameState.GameStateUpdateMessage;
 import nl.fontys.s3.indi.diamond_director_be.domain.GameState.ScoreRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -12,12 +15,22 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @CrossOrigin(origins = "http://127.0.0.1:5173/")
 public class ScoreController {
-    private final SimpMessagingTemplate messagingTemplate;
-
+    private final AddNewBaseballPlayUseCase addNewBaseballPlayUseCase;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @PostMapping
     public ResponseEntity<Void> addNewGameScore(@RequestBody @Valid ScoreRequest scoreRequest) {
+        Game updatedGame = addNewBaseballPlayUseCase.createNewPlayAndScore(scoreRequest);
 
+        GameStateUpdateMessage message = GameStateUpdateMessage.builder()
+                .awayScore(updatedGame.getAwayScore())
+                .homeScore(updatedGame.getHomeScore())
+                .awayTeamName(updatedGame.getAway().getTeamName())
+                .homeTeamName(updatedGame.getHome().getTeamName())
+                .outs(updatedGame.getOuts())
+                .build();
+
+        simpMessagingTemplate.convertAndSend("/topic/game/"+updatedGame.getId(), message);
         return ResponseEntity.ok().build();
     }
 }
