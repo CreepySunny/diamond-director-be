4c181d0fdad1e6255c1786731657462c3265dc69
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 05:57:32 +0200

    Renamed FindCoachesResponse

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/domain/Coach/FindCoachesResponse.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/domain/Coach/FindCoachesResponse.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class FindCoachesFromTeamNameResponse {
+public class FindCoachesResponse {
     private HttpStatusCodeException exception;
     private List<Coaches> coaches;
 }
