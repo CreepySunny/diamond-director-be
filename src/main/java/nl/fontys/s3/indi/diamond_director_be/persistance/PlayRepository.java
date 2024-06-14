66866a75b21eca4228d796028b6cf2fc7e829c04
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 02:28:53 +0200

    Find play by batter id

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/PlayRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/PlayRepository.java
@@ -3,5 +3,8 @@ package nl.fontys.s3.indi.diamond_director_be.persistance;
 import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.PlayEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlayRepository extends JpaRepository<PlayEntity, Long> {
+    List<PlayEntity> findByBatterId(Long batterId);
 }
