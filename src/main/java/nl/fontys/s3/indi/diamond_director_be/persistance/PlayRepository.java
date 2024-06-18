c17a97a4aa2b50f8fd25ae38b6bdb5a7653a837a
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 05:41:48 +0200

    Find by batter and pitcher

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/PlayRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/PlayRepository.java
@@ -1,10 +1,13 @@
 package nl.fontys.s3.indi.diamond_director_be.persistance;
 
 import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.PlayEntity;
+import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PlayRepository extends JpaRepository<PlayEntity, Long> {
-    List<PlayEntity> findByBatterId(Long batterId);
+    List<PlayEntity> findByBatter(PlayerEntity playerEntity);
+
+    List<PlayEntity> findByPitcher(PlayerEntity playerEntity);
 }
