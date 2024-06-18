008e1c5b6f91e2e55ef55f4ed2a4fb81ce2fe4e0
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 05:49:59 +0200

    Get all players from game id.

---
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/business/GetAllPlayersFromGameIdUseCase.java
@@ -0,0 +1,9 @@
+package nl.fontys.s3.indi.diamond_director_be.business;
+
+import nl.fontys.s3.indi.diamond_director_be.domain.Player.Player;
+
+import java.util.List;
+
+public interface GetAllPlayersFromGameIdUseCase {
+    List<Player> getAllPlayersFromGameId(Long gameId);
+}
