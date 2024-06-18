b9895a4a7e648d43470c9b69d83a256ccd7a9a34
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 05:41:21 +0200

    Find players no team

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/PlayerRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/PlayerRepository.java
@@ -4,7 +4,9 @@ import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
-
+    List<PlayerEntity> findByTeamIsNull();
 }
