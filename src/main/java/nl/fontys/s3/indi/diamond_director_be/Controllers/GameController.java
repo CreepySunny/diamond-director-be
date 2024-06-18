377aafbaaa9a4ee491d8fc54b77ab93c2c0b8856
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 05:27:28 +0200

    Http status import

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/Controllers/GameController.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/Controllers/GameController.java
@@ -7,6 +7,7 @@ import nl.fontys.s3.indi.diamond_director_be.business.*;
 import nl.fontys.s3.indi.diamond_director_be.domain.GameState.*;
 import nl.fontys.s3.indi.diamond_director_be.domain.Player.Player;
 import nl.fontys.s3.indi.diamond_director_be.persistance.GameRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
