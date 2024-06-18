c62e83e6182c2b38c68cefc418b0cee7e6ec64e1
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 05:33:32 +0200

    Unused import

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/CoachRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/CoachRepository.java
@@ -1,7 +1,6 @@
 package nl.fontys.s3.indi.diamond_director_be.persistance;
 
 import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.CoachEntity;
-import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
