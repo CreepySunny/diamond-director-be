d2b3caf69df1cf5ba92720b3fdc69d03f97cd5be
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 05:46:52 +0200

    Formatting

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/UserRepository.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/persistance/UserRepository.java
@@ -1,10 +1,8 @@
 package nl.fontys.s3.indi.diamond_director_be.persistance;
 
-import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.CoachEntity;
 import nl.fontys.s3.indi.diamond_director_be.persistance.Entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
