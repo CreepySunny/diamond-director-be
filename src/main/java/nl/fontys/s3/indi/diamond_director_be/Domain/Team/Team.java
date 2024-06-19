ea95243a9a6793e80494cf20b6024d66c63c1db4
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-19 15:13:55 +0200

    Added team id

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/Domain/Team/Team.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/Domain/Team/Team.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class Team {
+    private Long id;
     List<Coaches> coaches;
     List<Player> rooster;
     private String teamName;
