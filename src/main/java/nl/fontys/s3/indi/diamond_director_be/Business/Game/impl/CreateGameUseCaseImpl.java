656c0e17759ff16ee221ccc61957b46f81f45e0e
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-21 11:18:13 +0200

    Added inning half to game entity

--- src/main/java/nl/fontys/s3/indi/diamond_director_be/Business/Game/impl/CreateGameUseCaseImpl.java
+++ src/main/java/nl/fontys/s3/indi/diamond_director_be/Business/Game/impl/CreateGameUseCaseImpl.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import nl.fontys.s3.indi.diamond_director_be.Business.Game.CreateGameUseCase;
 import nl.fontys.s3.indi.diamond_director_be.Business.Team.Exceptions.NO_TEAM_FOUND_EXCEPTION;
 import nl.fontys.s3.indi.diamond_director_be.Domain.GameState.CreateGameRequest;
+import nl.fontys.s3.indi.diamond_director_be.Domain.GameState.Enums.InningHalves;
 import nl.fontys.s3.indi.diamond_director_be.Persistance.Entities.GameEntity;
 import nl.fontys.s3.indi.diamond_director_be.Persistance.Entities.TeamEntity;
 import nl.fontys.s3.indi.diamond_director_be.Persistance.GameRepository;
@@ -22,6 +23,7 @@ public class CreateGameUseCaseImpl implements CreateGameUseCase {
                 .season(request.getSeason())
                 .homeTeam(findTeamByName(request.getHomeTeamName()))
                 .awayTeam(findTeamByName(request.getAwayTeamName()))
+                .halves(InningHalves.TOP)
                 .awayScore(0)
                 .homeScore(0)
                 .outs(0)
